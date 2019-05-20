f24c7b14a090b1642f5b2e935c23f5524fee4711
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-05-20 13:56:52 +0200

    Init

---
+++ event.js
@@ -0,0 +1,10 @@
+const Event = {
+  ANSWER:           'receiver.join.answer',
+  RECEIVER_UPGRADE: 'receiver.upgrade',
+  OFFER:            'receiver.join.offer',
+  NOT_FOUND:        'receiver.join.initiator.notfound',
+  CLIENT_ID:        'client.id',
+  HEARTBEAT:        'heartbeat',
+}
+
+module.exports = Event
---
+++ index.js
@@ -0,0 +1,9 @@
+const runInitiator = require('./runInitiator')
+const runReceiver = require('./runReceiver')
+const Event = require('./event')
+
+module.exports = {
+  runInitiator,
+  runReceiver,
+  Event,
+}
