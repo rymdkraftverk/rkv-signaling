d8c4cb0de6eaf7d1f5e87dfd668fc55e6267a73d
Lorentz Lasson <lasson.lorentz@gmail.com>
2019-05-26 15:16:26 +0200

    🐞 Exposing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+const common = require('./common')
 const runInitiator = require('./runInitiator')
 const runReceiver = require('./runReceiver')
 const Event = require('./event')
 
 module.exports = {
+  common,
   runInitiator,
   runReceiver,
   Event,
